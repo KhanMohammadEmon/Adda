4ae9749a586f87fc1921cae77b2277bf74137016
KHANMOHAMMADEMON <sagor.ms766@gmail.com>
2022-01-02 02:32:42 +0600

    Add some and singup some work and add css file edit by Emon

--- src/main/java/com/example/adda/OpenPageController.java
+++ src/main/java/com/example/adda/OpenPageController.java
@@ -8,12 +8,16 @@ import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.RadioButton;
+import javafx.scene.control.TextField;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.util.ArrayList;
 
 
 public class OpenPageController {
@@ -30,7 +34,43 @@ public class OpenPageController {
     @FXML
     public ImageView btnBack;
 
+    @FXML
+    public Button getStarted;
 
+    @FXML
+    public TextField regName;
+    @FXML
+    public TextField regPass;
+    @FXML
+    public TextField regEmail;
+    @FXML
+    public TextField regFirstName;
+    @FXML
+    public TextField regPhoneNo;
+    @FXML
+    public RadioButton male;
+    @FXML
+    public RadioButton female;
+    @FXML
+    public Label controlRegLabel;
+    @FXML
+    public Label success;
+    @FXML
+    public Label goBack;
+    @FXML
+    public TextField userName;
+    @FXML
+    public TextField passWord;
+    @FXML
+    public Label loginNotifier;
+    @FXML
+    public Label nameExists;
+    @FXML
+    public Label checkEmail;
+
+    public static String username, password, gender;
+
+    public  static ArrayList<User> users = new ArrayList<User>();
     //image back btn Handel...
     @FXML
     private void handleMouseEvent(MouseEvent event) {
@@ -70,5 +110,83 @@ public class OpenPageController {
         }
     }
 
+    public  void registration()
+    {
+        if(!regName.getText().equalsIgnoreCase("")
+            && !regPass.getText().equalsIgnoreCase("")
+                && !regEmail.getText().equalsIgnoreCase("")
+                && !regFirstName.getText().equalsIgnoreCase("")
+                && !regPhoneNo.getText().equalsIgnoreCase("")
+                && (male.isSelected() || female.isSelected())
+
+        )
+        {
+            if(checkUser(regName.getText()))
+            {
+                if(checkEmail(regEmail.getText()))
+                {
+                    User newUser = new User();
+                    newUser.name = regName.getText();
+                    newUser.password = regPass.getText();
+                    newUser.email = regEmail.getText();
+                    newUser.fullName = regFirstName.getText();
+                    newUser.phoneNo = regPhoneNo.getText();
+
+                    if(male.isSelected())
+                    {
+                        newUser.gender= "Male";
+                    }
+                    else
+                    {
+                        newUser.gender= "Female";
+                    }
+                    users.add(newUser);
+                    goBack.setOpacity(1);
+                    success.setOpacity(1);
+                    makeDefault();
+                }
+            }
+        }
 
     }
+
+
+
+
+    private void setOpacity(Label controlRegLabel, Label checkEmail, Label nameExists) {
+        controlRegLabel.setOpacity(0);
+        checkEmail.setOpacity(0);
+        nameExists.setOpacity(0);
+    }
+
+    private void makeDefault() {
+        regName.setText("");
+        regPass.setText("");
+        regEmail.setText("");
+        regFirstName.setText("");
+        regPhoneNo.setText("");
+        male.setSelected(true);
+        setOpacity(controlRegLabel, checkEmail, nameExists); // need learn about setOpacity....
+    }
+
+    private boolean checkEmail(String email) {
+        for(User user : users) {
+            if(user.email.equalsIgnoreCase(email)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private boolean checkUser(String username)
+    {
+        for (User user: users) {
+            if(user.email.equalsIgnoreCase(username))
+            {
+                return false;
+            }
+
+        }
+        return true;
+    }
+}
---
+++ src/main/java/com/example/adda/User.java
@@ -0,0 +1,11 @@
+package com.example.adda;
+
+public class User {
+    public  String name;
+    public String fullName;
+    public  String password;
+    public String email;
+    public String gender;
+    public String phoneNo;
+
+}
